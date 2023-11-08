cc5cd8e0ccd1ccc54f12025acb8b726d00595e03
macarena <blancomacab@gmail.com>
2023-11-07 21:54:24 -0300

    simulando agregar al carrito

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-/*const promociones = [
-    {
 
-        titulo: "Cavado ",
-        descuento: "20% OFF",
-        precioAntes: "$4.000",
-        precioDespues: "$3.200",
-    },
-    {
-        titulo: "Axilas",
-        descuento: "20% OFF",
-        precioAntes: "$4.000",
-        precioDespues: "$3.200",
-    },
-    {
-        titulo: "Tiro de cola",
-        descuento: "20% OFF",
-        precioAntes: "$3.600",
-        precioDespues: "$2.880",
-    }
-];
 const contenedorPromociones = document.getElementById("contenedorPromociones");
 const contenedorItems = document.getElementById("itemsCarrito")
 function muestraCarrito() {
@@ -32,33 +12,6 @@ function agregarPromoAlCarrito(producto) {
     localStorage.setItem('carrito', JSON.stringify(carrito));
     
 }
-promociones.forEach(promo => {
-    const divPromocion = document.createElement("div");
-    divPromocion.classList.add("card");
-    divPromocion.innerHTML = `
-    <div class="contieneTitulo">
-        <h5 class="titulo">${promo.titulo}</h5>
-    </div>
-    <ul class="listadoPrecios">
-        <li class="tachado">${promo.descuento}</li>
-        <li class="precioFinal"><del>${promo.precioAntes}</del> ${promo.precioDespues}</li>
-    </ul>
-    <div class="card-body">
-    <button id= "agregar" class = "agregar">La quiero</button>
-</div>`
-
-    contenedorPromociones.appendChild(divPromocion);
-    const agregar = divPromocion.querySelector(".agregar")
-
-    agregar.addEventListener("click", () => {
-        agregarPromoAlCarrito(promo);
-        alert(`Producto agregado al carrito: ${promo.titulo} valor: ${promo.precioDespues}`);
-    });
-});
-
-
-*/
-const contenedorPromociones = document.getElementById("contenedorPromociones");
 
 fetch("../promos.json")
     .then(Response => Response.json())
@@ -81,5 +34,11 @@ fetch("../promos.json")
 
             contenedorPromociones.appendChild(divPromocion);
             const agregar = divPromocion.querySelector(".agregar")
+            agregar.addEventListener("click", () => {
+                agregarPromoAlCarrito(promo);
+                alert(`Producto agregado al carrito: ${promo.titulo} valor: ${promo.precioDespues}`);
+            });
+            
         })
+
     })
